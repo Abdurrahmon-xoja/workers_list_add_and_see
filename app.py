73e876e3d80a8b7ffce04bfe4929b3fe55029256
Abdurrahmon-xoja <abdurrrahmont@icloud.com>
2022-06-09 21:39:42 +0500

    added new type of importing

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from functions import add_workers
-from functions import see
+import  functions
 
 
 user_order = input('what you want to do: ')
@@ -8,7 +7,7 @@ if user_order == 'add':
     user_order_name = input('enter name: ')
     user_order_age = input('enter age: ')
     user_order_position = input('enter position: ')
-    add_workers(user_order_name, user_order_age, user_order_position)
+    functions.add_workers(user_order_name, user_order_age, user_order_position)
 
 elif user_order == 'see':
-    see()
+    functions.see()
