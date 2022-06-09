3deb904f8577822b0b845f58b6c2bc988683de05
Abdurrahmon-xoja <abdurrrahmont@icloud.com>
2022-06-09 21:05:34 +0500

    end writing see function

--- functions.py
+++ functions.py
@@ -10,6 +10,12 @@ def add_workers(name,age,position):
         with open('data.json', 'w') as file:
             json.dump(data, file, indent=6)
 
+def see():
+    with open('data.json', 'r') as file:
+        data = json.load(file)
+        array_in_data = data["workers"]
+        for w in array_in_data:
+            print(array_in_data["name"], array_in_data["age"], array_in_data["position"])
 
 
 
