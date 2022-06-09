f344f0cd91eff53c10f25fa55bb0c5ff689043b5
Abdurrahmon-xoja <abdurrrahmont@icloud.com>
2022-06-09 21:10:52 +0500

    prepared in app file all actions test1

--- app.py
+++ app.py
@@ -1,2 +1,14 @@
 from functions import add_workers
+from functions import see
 
+
+user_order = 'what you want to do: '
+
+if user_order == 'add':
+    user_order_name = input('enter name: ')
+    user_order_age = input('enter age: ')
+    user_order_position = input('enter position: ')
+    add_workers(user_order_name, user_order_age, user_order_position)
+
+elif user_order == 'see':
+    see()
